32d7b72e0172cc49e0342cd88121cb7abc3cd577
Chris Dombroski <cdombroski@icanttype.org>
2013-07-02 07:22:35 -0400

    travis fix again plus test against multiple clojure versions

--- project.clj
+++ project.clj
@@ -4,6 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :dev-dependencies [[lein-midje "3.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.0.0"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :main clangband.core)
