51ba6828deb6b8339076266791fde11404358468
cdombroski <cdombroski@icanttype.org>
2013-07-02 10:18:32 -0400

    Remove 1.3 profile

--- project.clj
+++ project.clj
@@ -5,9 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.0"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+                   :plugins [[lein-midje "3.0.1"]]}
+             :1.4 [:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}]
+             :1.5 [:dev {:dependencies [[org.clojure/clojure "1.5.0"]]}]
+             :1.5.1 [:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}]}
   :main clangband.core)
